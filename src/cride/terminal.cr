a9b54bab6e6d2e8dac3f2d6efa8f2137ee4ac578
Julien Reichardt <mi@jrei.ch>
2018-08-13 17:32:15 +0200

    Add handling for file write error

--- src/cride/terminal.cr
+++ src/cride/terminal.cr
@@ -36,23 +36,29 @@ struct Cride::Terminal
       @render.editor
       case (input = wait_input).type
       when Key::CTRL_C, Key::CTRL_Q, Key::Esc then break
-      when Key::CTRL_S                        then @editor.file.write
-      when Key::CTRL_D                        then @editor.add.duplicate_line
-      when Key::CTRL_K                        then @editor.file.rows[@editor.position.absolute_y].empty? ? @editor.delete.line : @editor.delete.clear_line
-      when Key::CTRL_H, Key::Backspace        then @editor.delete.back
-      when Key::CTRL_ArrowUp                  then @editor.move.previous_block
-      when Key::CTRL_ArrowDown                then @editor.move.next_block
-      when Key::CTRL_ArrowRight               then @editor.move.next_word
-      when Key::CTRL_ArrowLeft                then @editor.move.previous_word
-      when Key::ArrowUp                       then @editor.move.up
-      when Key::ArrowDown                     then @editor.move.down
-      when Key::ArrowRight                    then @editor.move.right
-      when Key::ArrowLeft                     then @editor.move.left
-      when Key::Delete                        then @editor.delete.forward
-      when Key::PageUp                        then @editor.move.page_up
-      when Key::PageDown                      then @editor.move.page_down
-      when Key::Enter                         then @editor.add.line
-      when Key::Insert                        then @editor.insert = !@editor.insert
+      when Key::CTRL_S
+        begin
+          @editor.file.write
+        rescue ex
+          reset
+          abort ex
+        end
+      when Key::CTRL_D                 then @editor.add.duplicate_line
+      when Key::CTRL_K                 then @editor.file.rows[@editor.position.absolute_y].empty? ? @editor.delete.line : @editor.delete.clear_line
+      when Key::CTRL_H, Key::Backspace then @editor.delete.back
+      when Key::CTRL_ArrowUp           then @editor.move.previous_block
+      when Key::CTRL_ArrowDown         then @editor.move.next_block
+      when Key::CTRL_ArrowRight        then @editor.move.next_word
+      when Key::CTRL_ArrowLeft         then @editor.move.previous_word
+      when Key::ArrowUp                then @editor.move.up
+      when Key::ArrowDown              then @editor.move.down
+      when Key::ArrowRight             then @editor.move.right
+      when Key::ArrowLeft              then @editor.move.left
+      when Key::Delete                 then @editor.delete.forward
+      when Key::PageUp                 then @editor.move.page_up
+      when Key::PageDown               then @editor.move.page_down
+      when Key::Enter                  then @editor.add.line
+      when Key::Insert                 then @editor.insert = !@editor.insert
       when Key::ValidString
         input.to_s.each_char do |char|
           if char == '\r'
@@ -68,7 +74,7 @@ struct Cride::Terminal
     # Essential to call shutdown to reset lower-level terminal flags
   rescue ex
     reset
-    puts <<-ERR
+    abort <<-ERR
     An error as occured. Please create an issue at https://github.com/j8r/cride with the steps to how reproduce this bug.
     
     cursor_y:  #{@editor.position.cursor_y + 1}, cursor_x: #{@editor.position.cursor_x + 1}
@@ -78,7 +84,6 @@ struct Cride::Terminal
     Backtrace:
     #{ex.backtrace.join('\n')}
     ERR
-    exit 1
   else
     reset
   end
